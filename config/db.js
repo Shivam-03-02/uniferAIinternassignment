15dc78eb633b4f948343ca7eecad1bfea943cd78
Shivam-03-02 <108723573+Shivam-03-02@users.noreply.github.com>
2024-06-12 19:44:47 +0530

    Update db.js
    connection string generalized

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/nodejs-auth-app');
+        await mongoose.connect('your_mongo_connection_string');
         console.log('MongoDB connected...');
     } catch (err) {
         console.error(err.message);
